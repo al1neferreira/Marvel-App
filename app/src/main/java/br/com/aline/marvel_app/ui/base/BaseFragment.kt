b35e632d56debd24841eca7f05ce9e672e4d224f
Aline Ferreira <alinfst@gmail.com>
2024-06-22 13:00:00 -0300

    setup room save and delete favorites

--- app/src/main/java/br/com/aline/marvel_app/ui/base/BaseFragment.kt
+++ app/src/main/java/br/com/aline/marvel_app/ui/base/BaseFragment.kt
@@ -9,15 +9,14 @@ import androidx.lifecycle.ViewModel
 import androidx.viewbinding.ViewBinding
 
 abstract class BaseFragment<VB : ViewBinding, VM : ViewModel> : Fragment() {
-
     private var _binding: VB? = null
-    protected val binding get() = _binding!!
+    protected val binding get()=_binding!!
     protected abstract val viewModel: VM
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
         _binding = getViewBinding(inflater, container)
         return binding.root
